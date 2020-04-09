30e9c7940e054373447271244fc28a93d2e10069
marishwaran <marishwaran.k@somaiya.edu>
2020-04-09 12:13:37 +0530

    added bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:toys_shop/pages/add_to_cart_page.dart';
+import 'package:toys_shop/pages/home_page.dart';
+import 'package:toys_shop/pages/product_page.dart';
 import 'package:toys_shop/widgets/SectionTitle.dart';
 import 'package:toys_shop/widgets/appbar.dart';
+import 'package:toys_shop/widgets/bottom_appbar.dart';
 import 'package:toys_shop/widgets/home_page_carousel.dart';
 import 'package:toys_shop/widgets/in_section_spacing.dart';
 import 'package:toys_shop/widgets/product_carousel.dart';
@@ -14,43 +18,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Toys Shop',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Josefin'
-      ),
-      home: ProductPage(),
+      theme: ThemeData(fontFamily: 'Josefin'),
+      home: MainPage(),
     );
   }
 }
 
-class ProductPage extends StatefulWidget {
-  ProductPage({Key key}) : super(key: key);
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
 
   @override
-  _ProductPageState createState() => _ProductPageState();
+  _MainPageState createState() => _MainPageState();
 }
 
-class _ProductPageState extends State<ProductPage> {
+class _MainPageState extends State<MainPage> {
+  int _index = 0;
+  List<BarItem> _bottomBarItem = [
+    BarItem(title: 'Home', iconData: Icons.home),
+    BarItem(title: 'Shop', iconData: Icons.shopping_basket),
+    BarItem(title: 'Cart', iconData: Icons.shopping_cart),
+  ];
+  final List<Widget> _screens = [
+    HomePage(),
+    ProductPage(),
+    AddToCartPage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:MyAppBar(back:false),
-      body: SingleChildScrollView(
-        child:Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SectionSpacing(),
-              SectionTitle("Best Sellers"),
-              InSectionSpacing(),
-              HomePageCarousel(),
-              SectionSpacing(),
-              SectionTitle("Top Products"),
-              InSectionSpacing(),
-              ProductCarousel()
-
-            ],
-          ),
-        )
-    ));
+        appBar: MyAppBar(back: false),
+        body: _screens[_index],
+        bottomNavigationBar: AnimatedBottomBar(
+            barItems: _bottomBarItem,
+            duration: Duration(milliseconds: 200),
+            onBarTap: (i) {
+              setState(() {
+                _index = i;
+              });
+            }));
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:toys_shop/widgets/SectionTitle.dart';
+import 'package:toys_shop/widgets/home_page_carousel.dart';
+import 'package:toys_shop/widgets/in_section_spacing.dart';
+import 'package:toys_shop/widgets/product_carousel.dart';
+import 'package:toys_shop/widgets/section_spacing.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SectionSpacing(),
+          SectionTitle("Best Sellers"),
+          InSectionSpacing(),
+          HomePageCarousel(),
+          SectionSpacing(),
+          SectionTitle("Top Products"),
+          InSectionSpacing(),
+          ProductCarousel()
+        ],
+      ),
+    ));
+  }
+}
